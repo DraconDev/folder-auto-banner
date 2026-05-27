eefc145da6427721851d750612f93155b4143b84
DraconDev <dracsharp@gmail.com>
2026-05-27 14:19:46 +0100

    Modified src/cmd/banner.rs
    1 file changed:
      Modified: src/cmd/banner.rs

--- src/cmd/banner.rs
+++ src/cmd/banner.rs
@@ -98,9 +98,9 @@ fn output_rich(path: &Path, summary: &DirSummary, git_info: &GitInfo, _compact: 
     };
     
     println!("{}", header);
-    println!("{}", "─".repeat(85));
-    println!("{:<4}│ {:<30} │ {:>10} │ {:<8} │ {:<15}", " ", "NAME", "COUNT/SIZE", "TYPE", "MODIFIED");
-    println!("{}", "─".repeat(85));
+    println!("{}", "─".repeat(80));
+    println!("   │{:<30}│{:>10}│{:<8}│{:^12}│{:>15}", "NAME", "COUNT/SIZE", "TYPE", "", "MODIFIED");
+    println!("{}", "─".repeat(80));
     
     let mut visible_items: Vec<&crate::fs::DirEntry> = Vec::new();
     let mut hidden_items: Vec<&crate::fs::DirEntry> = Vec::new();
@@ -134,12 +134,12 @@ fn output_rich(path: &Path, summary: &DirSummary, git_info: &GitInfo, _compact: 
             let count = count_items_in_dir(item);
             let count_str = if count == 1 { "1" } else { &format!("{}", count) };
             let modified = item.modified.map(|dt| format_relative_time(&dt)).unwrap_or_default();
-            println!("📂│{:<30}│{:>5} items│{:<8}│{}", item.name, count_str, "[DIR]", modified);
+            println!("📂 │{:<30}│{:>5} items│{:<8}│{:^12}│{}", item.name, count_str, "[DIR]", "", modified);
         } else {
             let size = format_size_compact(item.size);
             let ext = get_extension_label(item);
             let modified = item.modified.map(|dt| format_relative_time(&dt)).unwrap_or_default();
-            println!("📄│{:<30}│{:>8}│{:<8}│{}", item.name, size, ext, modified);
+            println!("📄 │{:<30}│{:>8}│{:<8}│{:^12}│{}", item.name, size, ext, "", modified);
         }
     }
     
