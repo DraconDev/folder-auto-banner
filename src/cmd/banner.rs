a32dd470d7a36eed04bb4670156be8ce5c0f66ef
DraconDev <dracsharp@gmail.com>
2026-06-03 22:38:49 +0100

    1 file(s) in src [src/cmd/banner.rs] DELTA:+1/-1

--- src/cmd/banner.rs
+++ src/cmd/banner.rs
@@ -576,7 +576,7 @@ fn output_rich(path: &Path, summary: &DirSummary, git_info: &GitInfo, opts: &Ban
                 } else {
                     format!("{}d ago", diff / 86400)
                 };
-                parts.push(format!("{}", time_str));
+                parts.push(time_str);
             }
             // Commits today
             if git_info.commits_today > 0 {
