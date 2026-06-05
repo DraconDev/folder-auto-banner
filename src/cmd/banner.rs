2409e09c9107057a3a5efa9806f5eb2ea37dfc47
DraconDev <dracsharp@gmail.com>
2026-06-05 11:32:39 +0100

    2 file(s) in .pi,src [.pi/goals/active_goal_2026060511320603_mq0sbp8w-cf0l9p.md, src/cmd/banner.rs] DELTA:+35/-1 | NEW:goals/active_goal_2026060511320603_mq0sbp8w-cf0l9p.md

--- src/cmd/banner.rs
+++ src/cmd/banner.rs
@@ -1562,7 +1562,7 @@ fn output_rich(path: &Path, summary: &DirSummary, git_info: &GitInfo, opts: &Ban
         if config.numbered {
             let num = idx + 1; // 1-based numbering
             let num_padded = format!("{:>width$}", num, width = num_width);
-            let num_str = format!("{}[{}{}{}]{}", color(DIM), color(BOLD), color(CYAN), num_padded, color(RESET));
+            let num_str = format!("{}[{}{}{}]{}", color(DIM), color(BOLD), color(MAGENTA), num_padded, color(RESET));
             row_parts.push(num_str);
         }
         row_parts.push(icon_str);
